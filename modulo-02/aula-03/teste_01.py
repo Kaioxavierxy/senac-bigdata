400747f1ce754530ebf9113945e9ff31792603ed
Kaio xavier <dm.xavierkaio@gmail.com>
2026-09-16 21:39:00 -0300

    mandando arquivos da aula 16/09/2026

---
+++ modulo-02/aula-03/teste_01.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+# 1. Conectar ao banco de dados
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="meu_ecomerce"
+)
+
+# 2. Criar um objeto cursor para executar as queries
+cursor = conexao.cursor()
+
+# 3. Definir a query
+query = "SELECT * FROM produtos"
+
+# 4. Executar a query
+cursor.execute(query)
+
+# 5. Obter os resultados
+resultados = cursor.fetchall()
+
+# 6. Exibir os resultados
+for linha in resultados:
+    print(linha)
