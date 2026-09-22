030e7acb4ea5f05d42c6ff1088d512663cc44808
Kaio xavier <dm.xavierkaio@gmail.com>
2026-09-21 21:35:29 -0300

    mandando resultados finais

--- modulo-02/aula-03/aula03.py
+++ modulo-02/aula-03/aula03.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_csv('vendas_produtos.csv')
 print(df.head())
 
-######################
+#####################
 
 import mysql.connector
 
@@ -12,7 +12,7 @@ conexao = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="vendas_online2"
+    database="meu_ecomerce"
 )
 
 # 2. Criar um objeto cursor para executar as queries
@@ -36,33 +36,3 @@ cursor.close()
 conexao.close()
 
 ######################
-
-import mysql.connector
-
-def obter_dados_do_banco(query):
-    try:
-        conexao = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="vendas_online"
-        )
-        cursor = conexao.cursor()
-        cursor.execute(query)
-        resultados = cursor.fetchall()
-        return resultados
-    except mysql.connector.Error as erro:
-        print(f"Erro ao conectar ao MySQL: {erro}")
-        return None
-    finally:
-        if 'conexao' in locals() and conexao.is_connected():
-            cursor.close()
-            conexao.close()
-
-# Usando a função
-query_produtos = "SELECT * FROM produtos WHERE preco > 100"
-dados_filtrados = obter_dados_do_banco(query_produtos)
-
-if dados_filtrados:
-    for produto in dados_filtrados:
-        print(produto)
